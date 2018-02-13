19eaee0252c8153bbb0a50bb68d62d8f0aab3ecc
Guillaume KLECH <gklech@tessarine.com>
2018-02-13 21:22:48 +0100

    4.1 Refactor

--- src/main/java/org/klechcorp/stringcalculator/StringCalculator.java
+++ src/main/java/org/klechcorp/stringcalculator/StringCalculator.java
@@ -4,27 +4,32 @@ import java.util.StringTokenizer;
 
 public class StringCalculator {
 
+	private static final String CUS_DELIM_ENDER = "\n";
+	private static final String CUS_DELIM_HEADER = "//";
 	private static final String SEPARATOR = ",\n";
 
 	public static int add(String _numbers) {
 		if (_numbers.length() == 0) {
 			return 0;
-		} else if (_numbers.contains("//")) {
-			Character customDelim = _numbers.charAt(_numbers.indexOf("//") + "//".length());
-			StringTokenizer tok = new StringTokenizer(_numbers.substring(_numbers.indexOf("\n") +1 ), customDelim.toString());
-			int sum = 0;
-			while (tok.hasMoreTokens()) {
-				sum += Integer.parseInt(tok.nextToken());
-			}
-			return sum;
+		} else if (_numbers.contains(CUS_DELIM_HEADER)) {
+			int indOfCustomDelim = _numbers.indexOf(CUS_DELIM_HEADER) + CUS_DELIM_HEADER.length();
+			Character customDelim = _numbers.charAt(indOfCustomDelim);
+			int indOfNbrs = _numbers.indexOf(CUS_DELIM_ENDER) + CUS_DELIM_ENDER.length();
+			String realNumbers = _numbers.substring(indOfNbrs);
+			
+			return addNbrsTok(customDelim.toString(), realNumbers);
 			
 		} else {
-			StringTokenizer tok = new StringTokenizer(_numbers, SEPARATOR);
-			int sum = 0;
-			while (tok.hasMoreTokens()) {
-				sum += Integer.parseInt(tok.nextToken());
-			}
-			return sum;
+			return addNbrsTok(SEPARATOR, _numbers);
 		}
+	}
+
+	protected static int addNbrsTok(String customDelim, String realNumbers) {
+		StringTokenizer tok = new StringTokenizer(realNumbers, customDelim);
+		int sum = 0;
+		while (tok.hasMoreTokens()) {
+			sum += Integer.parseInt(tok.nextToken());
+		}
+		return sum;
 	}
 }
