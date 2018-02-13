92d854e4802a0eb7f3af86605c3cab68675bda04
Guillaume KLECH <gklech@tessarine.com>
2018-02-13 21:16:08 +0100

    4.1

--- src/main/java/org/klechcorp/stringcalculator/StringCalculator.java
+++ src/main/java/org/klechcorp/stringcalculator/StringCalculator.java
@@ -9,6 +9,15 @@ public class StringCalculator {
 	public static int add(String _numbers) {
 		if (_numbers.length() == 0) {
 			return 0;
+		} else if (_numbers.contains("//")) {
+			Character customDelim = _numbers.charAt(_numbers.indexOf("//") + "//".length());
+			StringTokenizer tok = new StringTokenizer(_numbers.substring(_numbers.indexOf("\n") +1 ), customDelim.toString());
+			int sum = 0;
+			while (tok.hasMoreTokens()) {
+				sum += Integer.parseInt(tok.nextToken());
+			}
+			return sum;
+			
 		} else {
 			StringTokenizer tok = new StringTokenizer(_numbers, SEPARATOR);
 			int sum = 0;
--- src/test/java/org/klechcorp/stringcalculator/StringCalculatorTest.java
+++ src/test/java/org/klechcorp/stringcalculator/StringCalculatorTest.java
@@ -44,6 +44,9 @@ public class StringCalculatorTest {
 		assertEquals(6, StringCalculator.add("1\n,2,3"));
 	}
 	
-	
+	@Test
+	public void supportCustomDelimiter() throws Exception {
+		assertEquals(6, StringCalculator.add("//;\n1;2;3"));
+	}
 
 }
